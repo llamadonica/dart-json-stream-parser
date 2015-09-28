f5f83860f9d538e8bb1186923ee191bf59d867cf
Adam Stark <llamadonica@gmail.com>
2015-09-27 22:19:33 -0700

    Add testing rig.

---
+++ run_tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+if [ "$COVERALLS_TOKEN" ]; then
+  # run tests on travis and publish code coverage
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $COVERALLS_TOKEN \
+    --retry 2 \
+    --exclude-test-files \
+    test/geohash_test.dart
+else
+  # run tests locally
+  dart --checked test/json_stream_parser_test.dart
+fi
